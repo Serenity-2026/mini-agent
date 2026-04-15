6a82483a8f12b2956cfebcf6fb976702b493ee45
dengminchuan <1755875003@qq.com>
2026-04-15 15:50:41 +0800

    mini-agent1.0测试成功，正式发布

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,12 @@ import OpenAI from "openai";
 import dotenv from 'dotenv';
 import ChatOpenAI from "./ChatOpenAI";
 import MCPClient from "./MCPClient";
+import Agent from "./Agent";
 dotenv.config();
-const openai = new OpenAI({
-    baseURL: process.env.OPENAI_BASE_URL,
-    apiKey: process.env.OPENAI_API_KEY
-});
-
 async function main() {
     // testChat()
-    testToolCall()
+    //testToolCall()
+    testAgent()
 }
 
 main();
@@ -25,4 +22,13 @@ async function testToolCall() {
     const tools=fetchMcp.getTools()
     console.log(tools)
     await fetchMcp.close()
+}
+async function testAgent(){
+    const currentDir=process.cwd()
+    const fetchMcp=new MCPClient('fetch','uvx',['mcp-server-fetch'])
+    const fileMcp=new MCPClient('file','npx',["-y","@modelcontextprotocol/server-filesystem",currentDir])
+    const agent=new Agent([fetchMcp,fileMcp])
+    await agent.init()
+    const response=await agent.invoke(`爬取https://www.zhihu.com/的内容,并且总结后保存到${currentDir}的news.md文件夹`)
+    console.log(response)
 }
