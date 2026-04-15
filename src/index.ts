e279b402d40589bb2e5a052b6f02141ba8254c6b
dengminchuan <1755875003@qq.com>
2026-04-15 11:29:10 +0800

    add:添加MCP server测试

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import OpenAI from "openai";
 import dotenv from 'dotenv';
 import ChatOpenAI from "./ChatOpenAI";
+import MCPClient from "./MCPClient";
 dotenv.config();
-console.log(process.env.OPENAI_API_KEY)
 const openai = new OpenAI({
     baseURL: process.env.OPENAI_BASE_URL,
     apiKey: process.env.OPENAI_API_KEY
 });
 
 async function main() {
-    await new ChatOpenAI("deepseek-chat", "you are a teacher").chat("tell me a joke")
+    //testChat()
+    testToolCall()
 }
 
 main();
 
+async function testChat() {
+    await new ChatOpenAI("deepseek-chat", "you are a teacher").chat("tell me a joke")
+}
+
+async function testToolCall() {
+    const fetchMcp=new MCPClient('fetch','uvx',['mcp-server-fetch'])
+    await fetchMcp.init()
+    const tools=fetchMcp.getTools()
+    await fetchMcp.close()
+}
